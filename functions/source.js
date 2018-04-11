f16da50900b7f9b4b2f1238d83a301a0b27c15ab
Gabe Rodriguez <gabe@spotify.com>
2018-04-11 16:35:03 +0200

    Updated cloud function to async/await

--- functions/source.js
+++ functions/source.js
@@ -14,96 +14,95 @@ app.get("/helloworld", (req, res) => {
 });
 
 app.get("/webhook", (req, res) => {
-  if (req.query["hub.verify_token"] === functions.config().myservicekeys.fbtoken) {
+  if (
+    req.query["hub.verify_token"] === functions.config().myservicekeys.fbtoken
+  ) {
     res.send(req.query["hub.challenge"]);
   } else {
     res.send("No sir.");
   }
 });
 
-app.get("/test", async (req, res) => {
+// Change made to TMS FB page :: Update FBData.json in githubrepo
+app.post("/webhook", async (req, res) => {
+  const githubToken = functions.config().myservicekeys.githubtoken;
   try {
-    const website = await rp('http://www.microsoft.com/')
-    return res.send(website);
-  }
-  catch (err) {
-    console.log('fetch failed', err);
-    return err;
+    const postInfo = await getMostRecentPost();
+    const githubFBJson = await getFBDataFromGithub(githubToken);
+    const response = await updateGithubFBData(
+      githubFBJson,
+      postInfo,
+      githubToken
+    );
+    return res.send(response);
+  } catch (err) {
+    console.log("Something failed!", err);
+    return res.send(err);
   }
 });
 
-// Change made to TMS FB page :: Update FBData.json in githubrepo
-app.post("/webhook", (req, res) => {
+const getMostRecentPost = async () => {
   const fbSecret = functions.config().myservicekeys.fbappsecret;
-  let options = {
+  const options = {
     uri: `https://graph.facebook.com/v2.8/790534394301792/feed?fields=permalink_url,from,message,full_picture&access_token=1828570360690824|${fbSecret}`,
     json: true
   };
+  const response = await rp(options);
+  const postInfo = response.data.filter(
+    post => post.from.name === "The Meatball Stoppe" && post.full_picture
+  )[0];
+  postInfo.message = postInfo.message
+    ? postInfo.message.slice(0, 115) + "..."
+    : "";
+  return postInfo;
+};
 
-  rp(options).then(response => {
-    const postInfo = response.data.filter(post => {
-      return post.from.name === "The Meatball Stoppe" && post.full_picture;
-    })[0];
-
-    if (postInfo.message) {
-      postInfo.message = postInfo.message.slice(0, 115) + "...";
-    } else {
-      postInfo.message = "";
+const getFBDataFromGithub = async githubToken => {
+  const options = {
+    uri: `https://api.github.com/repos/grod220/TMS-D.A.Carson/contents/src/components/homepage/socialBar/livePost/FBData.json?access_token=${githubToken}`,
+    json: true,
+    headers: {
+      "User-Agent": "Request-Promise"
     }
-    return postInfo;
-  }).then(postInfo => {
-    const githubToken = functions.config().myservicekeys.githubtoken;
-    const options = {
-      uri: `https://api.github.com/repos/grod220/TMS-D.A.Carson/contents/src/components/homepage/socialBar/livePost/FBData.json?access_token=${githubToken}`,
-      json: true,
-      headers: {
-        "User-Agent": "Request-Promise"
-      }
-    };
-    return Promise.all([rp(options), postInfo, githubToken]);
-  }).then(([gResponse, postInfo, githubToken]) => {
-    const newJSONcontent = {
-      mostRecentFBPost: {
-        imageURL: postInfo.full_picture,
-        message: postInfo.message,
-        url: postInfo.permalink_url
-      }
-    };
+  };
+  return rp(options);
+};
 
-    const newObjJsonStr = JSON.stringify(newJSONcontent, null, 2);
-    const newObjJsonB64 = Buffer.from(newObjJsonStr).toString("base64");
-
-    if (newObjJsonB64 === gResponse.content.replace(/\n/g, "")) {
-      return "Post already published on site.";
+const updateGithubFBData = async (githubFBJson, postInfo, githubToken) => {
+  const newJSONcontent = {
+    mostRecentFBPost: {
+      imageURL: postInfo.full_picture,
+      message: postInfo.message,
+      url: postInfo.permalink_url
     }
+  };
+  const newObjJsonStr = JSON.stringify(newJSONcontent, null, 2);
+  const newObjJsonB64 = Buffer.from(newObjJsonStr).toString("base64");
+  if (newObjJsonB64 === githubFBJson.content.replace(/\n/g, "")) {
+    return "Post already published on site.";
+  }
 
-    const newFileBody = {
-      message: `Updated FB post content on ${new Date()}`,
-      committer: {
-        name: "Mr. Robot",
-        email: "mrrobot-fake-email@firebase.com"
-      },
-      content: newObjJsonB64,
-      sha: gResponse.sha
-    };
+  const newFileBody = {
+    message: `Updated FB post content on ${new Date()}`,
+    committer: {
+      name: "Mr. Robot",
+      email: "mrrobot-fake-email@firebase.com"
+    },
+    content: newObjJsonB64,
+    sha: githubFBJson.sha
+  };
 
-    const options = {
-      method: "PUT",
-      uri: `https://api.github.com/repos/grod220/TMS-D.A.Carson/contents/src/components/homepage/socialBar/livePost/FBData.json?access_token=${githubToken}`,
-      json: true,
-      body: newFileBody,
-      headers: {
-        "User-Agent": "Request-Promise"
-      }
-    };
+  const options = {
+    method: "PUT",
+    uri: `https://api.github.com/repos/grod220/TMS-D.A.Carson/contents/src/components/homepage/socialBar/livePost/FBData.json?access_token=${githubToken}`,
+    json: true,
+    body: newFileBody,
+    headers: {
+      "User-Agent": "Request-Promise"
+    }
+  };
 
-    return rp(options);
-  }).then(lastRes => {
-    return res.send(lastRes);
-  }).catch(err => {
-    res.send(err);
-    console.log(err.body);
-  });
-});
+  return rp(options);
+};
 
 exports.socialWidgetFBApiHandling = functions.https.onRequest(app);
