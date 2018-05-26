811f2f99dee26fc2345fead019174f9dd7f31281
Gabe Rodriguez <gabe@spotify.com>
2018-05-26 10:15:15 +0200

    Removing firebase from client side code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './sharedUtilities/registerServiceWorker';
-import firebaseInit from './sharedUtilities/firebase';
 
-firebaseInit();
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
