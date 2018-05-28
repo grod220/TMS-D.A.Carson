7c11c6974dfb277025140cf03e79528220f504e7
Gabe Rodriguez <grod220@gmail.com>
2018-05-28 20:05:56 +0200

    Revert "Integrating react-snap and pre-rendering"
    This reverts commit 0e451abb036b710ca3098352cf28e62b8ca4e58b.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import React from "react";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./sharedUtilities/registerServiceWorker";
-import { hydrate, render } from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './sharedUtilities/registerServiceWorker';
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
