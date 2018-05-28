0e451abb036b710ca3098352cf28e62b8ca4e58b
Gabe Rodriguez <gabe@spotify.com>
2018-05-28 19:39:13 +0200

    Integrating react-snap and pre-rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './sharedUtilities/registerServiceWorker';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./sharedUtilities/registerServiceWorker";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 registerServiceWorker();
