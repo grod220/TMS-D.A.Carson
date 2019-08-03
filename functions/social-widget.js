ef63587c55ce6e2368fe0ef854647971732b6b34
Gabe Rodriguez <gabe@spotify.com>
2019-08-03 22:08:34 +0200

    Adding orderModule routes

---
+++ functions/social-widget.js
@@ -0,0 +1,113 @@
+const functions = require("firebase-functions");
+const app = require("express")();
+const bodyParser = require("body-parser");
+const rp = require("request-promise");
+const cors = require("cors");
+
+app.use(cors({ origin: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/helloworld", (req, res) => {
+  res.send("Hello from SocialWidget!");
+});
+
+app.get("/webhook", (req, res) => {
+  if (
+    req.query["hub.verify_token"] === functions.config().myservicekeys.fbtoken
+  ) {
+    res.send(req.query["hub.challenge"]);
+  } else {
+    res.send("No sir.");
+  }
+});
+
+// Change made to TMS FB page :: Update FBData.json in githubrepo
+app.post("/webhook", async (req, res) => {
+  const githubToken = functions.config().myservicekeys.githubtoken;
+  try {
+    const postInfo = await getMostRecentPost();
+    const githubFBJson = await getFBDataFromGithub(githubToken);
+    const response = await updateGithubFBData(
+      githubFBJson,
+      postInfo,
+      githubToken
+    );
+    return res.send(response);
+  } catch (err) {
+    console.log("Something failed!", err);
+    return res.send(err);
+  }
+});
+
+const getMostRecentPost = async () => {
+  const fbSecret = functions.config().myservicekeys.fbappsecret;
+  const options = {
+    uri: `https://graph.facebook.com/v2.8/790534394301792/feed?fields=permalink_url,from,message,full_picture&access_token=1828570360690824|${fbSecret}`,
+    json: true
+  };
+  const response = await rp(options);
+  const postInfo = response.data.filter(
+    post => post.from.name === "The Meatball Stoppe" && post.full_picture
+  )[0];
+  postInfo.message = postInfo.message
+    ? postInfo.message.slice(0, 115) + "..."
+    : "View the latest post on Facebook ⇗";
+  return postInfo;
+};
+
+const getFBDataFromGithub = async githubToken => {
+  const options = {
+    uri: `https://api.github.com/repos/grod220/TMS-D.A.Carson/contents/src/components/homepage/socialBar/livePost/FBData.json?access_token=${githubToken}`,
+    json: true,
+    headers: {
+      "User-Agent": "Request-Promise"
+    }
+  };
+  return rp(options); // think this needs await?
+};
+
+const updateGithubFBData = async (githubFBJson, postInfo, githubToken) => {
+  const newJSONcontent = {
+    mostRecentFBPost: {
+      imageURL: postInfo.full_picture,
+      message: postInfo.message,
+      url: postInfo.permalink_url
+    }
+  };
+  const newObjJsonStr = JSON.stringify(newJSONcontent, null, 2);
+  const newObjJsonB64 = Buffer.from(newObjJsonStr).toString("base64");
+  // need to do image comparisons
+  if (newObjJsonB64 === githubFBJson.content.replace(/\n/g, "")) {
+    console.log("Post already published on site.");
+    return "Post already published on site.";
+  }
+
+  const newFileBody = {
+    message: `Updated FB post content on ${new Date()}`,
+    committer: {
+      name: "Mr. Robot",
+      email: "mrrobot-fake-email@firebase.com"
+    },
+    content: newObjJsonB64,
+    sha: githubFBJson.sha
+  };
+
+  const options = {
+    method: "PUT",
+    uri: `https://api.github.com/repos/grod220/TMS-D.A.Carson/contents/src/components/homepage/socialBar/livePost/FBData.json?access_token=${githubToken}`,
+    json: true,
+    body: newFileBody,
+    headers: {
+      "User-Agent": "Request-Promise"
+    }
+  };
+
+  console.log(
+    "Making PUT request to Github. Updating FBDatafile with ",
+    newJSONcontent
+  );
+  return rp(options);
+};
+
+module.exports = functions.https.onRequest(app);
---
+++ functions/stripe/index.js
@@ -0,0 +1,46 @@
+const functions = require("firebase-functions");
+const app = require("express")();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const stripe = require("stripe")("sk_test_aHRehjwK9Qu8Iy9wtWSZ1TbW");
+const Utils = require("./utils");
+
+const Sentry = require("@sentry/node");
+Sentry.init({
+  dsn: "https://37c553e6cb9c4d7b91443519ccd448a9@sentry.io/1514998"
+});
+
+app.use(cors({ origin: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/helloworld", (req, res) => {
+  res.send("Hello from Stripe!");
+});
+
+app.post("/order", async (req, res) => {
+  try {
+    Utils.validateOrderOrThrow(req.body);
+
+    const sessionObj = {
+      payment_method_types: ["card"],
+      line_items: Utils.formatCart(
+        req.body.shoppingCart,
+        req.body.tip,
+        req.body.tax,
+        req.body.deliveryFee
+      ),
+      success_url: "http://localhost:3000/order/success", // prod vs dev switch?
+      cancel_url: "http://localhost:3000/order/cancelled",
+      payment_intent_data: Utils.formatPaymentIntentObj(req.body)
+    };
+
+    const stripeSession = await stripe.checkout.sessions.create(sessionObj);
+    res.json(stripeSession);
+  } catch (e) {
+    Sentry.captureException(e);
+    res.status(500).json({ error: e.toString() });
+  }
+});
+
+module.exports = functions.https.onRequest(app);
